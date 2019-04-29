256123e4c8a2a9a766b7c2d17f7dee9e7811e2b2
Tom Helmuth <thelmuth@hamilton.edu>
2019-04-28 22:58:55 -0400

    Created project 4 skeleton.

---
+++ project4.py
@@ -0,0 +1,76 @@
+"""
+PLEASE DOCUMENT HERE
+
+Usage: python3 project3.py DATASET.csv
+"""
+
+import csv, sys, random, math
+
+def read_data(filename, delimiter=",", has_header=True):
+    """Reads datafile using given delimiter. Returns a header and a list of
+    the rows of data."""
+    data = []
+    header = []
+    with open(filename) as f:
+        reader = csv.reader(f, delimiter=delimiter)
+        if has_header:
+            header = next(reader, None)
+        for line in reader:
+            example = [float(x) for x in line]
+            data.append(example)
+
+        return header, data
+
+def convert_data_to_pairs(data, header):
+    """Turns a data list of lists into a list of (attribute, target) pairs."""
+    pairs = []
+    for example in data:
+        x = []
+        y = []
+        for i, element in enumerate(example):
+            if header[i].startswith("target"):
+                y.append(element)
+            else:
+                x.append(element)
+        pair = (x, y)
+        pairs.append(pair)
+    return pairs
+
+def dot_product(v1, v2):
+    """Computes the dot product of v1 and v2"""
+    sum = 0
+    for i in range(len(v1)):
+        sum += v1[i] * v2[i]
+    return sum
+
+def logistic(x):
+    """Logistic / sigmoid function"""
+    val = 1 / (1 + math.e ** -x)
+    return val
+
+################################################################################
+### Neural Network code goes here
+
+
+
+
+
+def main():
+    header, data = read_data(sys.argv[1], ",")
+
+    pairs = convert_data_to_pairs(data, header)
+
+    # Note: add 1.0 to the front of each x vector to account for the dummy input
+    training = [([1.0] + x, y) for (x, y) in pairs]
+
+    # Check out the data:
+    for example in training:
+        print(example)
+
+    ### I expect the running of your program will work something like this;
+    ### this is not mandatory and you could have something else below entirely.
+    # nn = NeuralNetwork([3, 6, 3])
+    # nn.back_propagation_learning(training)
+
+if __name__ == "__main__":
+    main()
